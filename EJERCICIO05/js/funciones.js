d8f0febca812d6cc2d39fb96c1e8f90a3b2a7fa2
rguindi <raul_fv@hotmail.com>
2023-10-04 23:50:33 +0200

    AcabadoencasaComprobar DNI

--- EJERCICIO05/js/funciones.js
+++ EJERCICIO05/js/funciones.js
@@ -2,20 +2,24 @@ function comprobarDNI(dni){
 
     let correcto = true;
     let letra = dni.charAt(8);
-    console.log ("La ultima letra del String es " + letra);
+    console.log ("La ultima letra del Dni introducido es " + letra);
 
     let numero = dni.substring (0,8);
-    console.log ("El numero introducido por parámetro es " + numero);
+    console.log ("El DNI sin letra es " + numero);
 
     let letrasDNI = "TRWAGMYFPDXBNJZSQVHLCKE";
 
-        if (dni.length !=9) correcto = false;
+        if (dni.length !=9) {
+            correcto = false;
+            console.log ("El DNI introducido no tiene la longitud correcta");
+        }
         else {
                 let numerodeletra= parseInt (numero%23);
                 
                 let letracorrecta= letrasDNI.charAt(numerodeletra);
                 if (letra.toLocaleUpperCase()!=letracorrecta.toLocaleUpperCase()){
                     correcto = false;
+                    console.log ("La letra no se corresponde con ese numero");
         }
 
 
@@ -24,6 +28,11 @@ function comprobarDNI(dni){
 
                 }
             }
-    var datos = window.prompt ("Introduce tu DNI");
+    var resultado = comprobarDNI(window.prompt ("Introduce tu DNI"));
+
+    if (resultado) window.alert ("El formato de DNI es Correcto");
+    else window.alert ("Formato de DNI Incorrecto");
+
+
 
 
