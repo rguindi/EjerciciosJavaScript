76cb627110ec179ca25d3c79efde45abb6958ad5
rguindi <raul@hotmail.com>
2023-10-05 17:55:08 +0200

    añadirplantillas

---
+++ PlantillasSanti/js/funciones.js
@@ -0,0 +1,7 @@
+console.log ("Empieza el programa");
+function funcionescribir(){
+    const in1 = document.getElementById ("in1");
+    document.getElementById ("c2").innerHTML += in1.value;
+}
+
+//document.getElementById ("b1").addEventListener
