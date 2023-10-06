4dd3aad8513fb263498be22adcc6298f2e995504
rguindi <raul_fv@hotmail.com>
2023-10-06 12:14:45 +0200

    Rombo hueco Terminado en Casa

---
+++ EJERCICIO6/js/funciones.js
@@ -0,0 +1,75 @@
+
+
+function generarRombo (){
+
+    filas = document.getElementById ("in1").value;
+    console.log ("Numero de filas seleccionadas = " + filas);
+
+    filasmitad = filas/2;
+  
+    espaciosexternos = filasmitad-1;
+    espaciosinternos = 2;
+
+    
+        for (let index = 0; index < espaciosexternos; index++) {
+            document.getElementById("c2").innerHTML += ("&nbsp");
+         }
+            espaciosexternos--;
+            document.getElementById("c2").innerHTML += ("*<br>");
+
+            filasmitad--;
+
+            do{
+
+            for (let index = 0; index < espaciosexternos; index++) {
+                document.getElementById("c2").innerHTML += ("&nbsp");
+             }
+             espaciosexternos--;
+             document.getElementById("c2").innerHTML += ("*");
+
+             for (let id = 0; id < espaciosinternos; id++) {
+                document.getElementById("c2").innerHTML += ("&nbsp");
+             }
+             espaciosinternos+=2;
+             document.getElementById("c2").innerHTML += ("*<br>");
+             filasmitad--;
+            }while (filasmitad>0)
+
+            //Hasta aqui imprime la mitad del rombo. Ahora se hace a la inversa.
+
+            filasmitad = filas/2;
+  
+            espaciosexternos = 0;
+            espaciosinternos = filas-1;
+
+                    do{
+        
+                    for (let index = 0; index < espaciosexternos; index++) {
+                        document.getElementById("c2").innerHTML += ("&nbsp");
+                     }
+                     espaciosexternos++;
+                     document.getElementById("c2").innerHTML += ("*");
+        
+                     for (let id = 0; id < espaciosinternos; id++) {
+                        document.getElementById("c2").innerHTML += ("&nbsp");
+                     }
+                     espaciosinternos-=2;
+                     document.getElementById("c2").innerHTML += ("*<br>");
+                          filasmitad--;               
+                    }
+                    
+                    
+                    while (filasmitad>0)
+
+                    for (let index = 0; index < espaciosexternos; index++) {
+                        document.getElementById("c2").innerHTML += ("&nbsp");
+                     }
+                        espaciosexternos++;
+                        document.getElementById("c2").innerHTML += ("*<br>");
+            
+                        filasmitad++;
+}
+ 
+ 
+ 
+ 
