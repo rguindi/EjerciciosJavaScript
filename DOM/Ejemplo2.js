8e8a4bfb395ef8cd422b0c594cb0ecd60dbedd87
rguindi <raul_fv@hotmail.com>
2023-11-03 23:03:45 +0100

    Ejercicios DOM

---
+++ DOM/Ejemplo2.js
@@ -0,0 +1,10 @@
+console.log('EJERCICIOS SEGUNDA PARTE\n---------------------------');
+//const nodolipsum = document.getElementById('lipsum'); YA DECLARADO EN EL OTRO ARCHIVO
+console.log(nodolipsum.firstElementChild);
+console.log(nodolipsum.children[1]);
+
+const li = document.getElementsByTagName('li');
+console.log(li[li.length-1]);
+
+const label = document.getElementsByTagName('label');
+console.log(label[label.length-1]);
