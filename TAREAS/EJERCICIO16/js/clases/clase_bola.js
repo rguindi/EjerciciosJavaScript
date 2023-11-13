e3fe5846ea0870346d7eb1c0689753a548d0720e
rguindi <raul_fv@hotmail.com>
2023-11-13 12:28:40 +0100

    Ejercicio 16 conseguido

--- TAREAS/EJERCICIO16/js/clases/clase_bola.js
+++ TAREAS/EJERCICIO16/js/clases/clase_bola.js
@@ -1,3 +1,4 @@
+import { misFunciones } from "../libreria/misfunciones.js"; 
 export class Bola { 
 
     static arrayBolas = [];
@@ -5,35 +6,41 @@ export class Bola {
 
     constructor(radio, posX, posY, colorBola, linearGradient) { 
       
-        this.id= Bola.contador++,
+        this.id= misFunciones.formatearA3cifras(Bola.contador++),
         this.radio = radio,
         this. posX = posX,
         this.posY = posY,
         this.colorBola = colorBola,
         this.linearGradient = linearGradient
         
-        Bola.arrayBolas.push= this;
+        Bola.arrayBolas.push(this) ;
     } 
-   
-    //  get arrayBolas() {
-    //     return Bola.arraybolas;
-    // }
+    
+
 
     visualizar(){
+        console.log('log por cacda bola');
    
         const div = document.createElement ('div');
-        div.style.width = this.radio *2;
-        div.style.height = this.radio *2;
-        div.setAttribute ('BorderRadius', "50%");
+        div.setAttribute ('id', 'idBola' + this.id);   
+        div.style.width = this.radio *2 + "px";
+        div.style.height = this.radio *2 + "px";
+        div.style.borderRadius = "50%";
         div.style.position = 'Absolute';
         div.style.top = this.posY + "px";
         div.style.left = this.posX + "px";
         div.style.backgroundColor = this.colorBola;
+
+        if (this.linearGradient !== undefined){
+            div.removeAttribute('style.backgroundColor')
+        }
         div.style.background = this.linearGradient;
+        document.body.appendChild (div);
+
     }
         
 
-    // }
+   
 
 
 } 
