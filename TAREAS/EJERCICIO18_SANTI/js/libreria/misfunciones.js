b488ca946ab7c9b6870113ecdddad2a127ef5022
rguindi <raul_fv@hotmail.com>
2023-11-17 13:41:08 +0100

    Acabado

---
+++ TAREAS/EJERCICIO18_SANTI/js/libreria/misfunciones.js
@@ -0,0 +1,92 @@
+import { Bola } from "../clases/clase_bola.js";
+let intervalo;
+function formatearA3cifras(num){
+    return num.toLocaleString(undefined,
+        { minimumIntegerDigits: 3, useGrouping: false}); //dejo useGrouping para recordar la propiedad);
+}
+
+function generarRandomInt(max){
+    return Math.floor(Math.random() * max);
+}
+
+function crearBotonCentrado(){
+    const boton=document.createElement('input');
+    boton.setAttribute('type', 'button');
+    boton.setAttribute('style', `position: absolute`);
+    boton.value='Booola';
+    boton.style.top=`${window.innerHeight - 50}px`
+    boton.style.left=`${window.innerWidth/2}px`
+    document.body.appendChild(boton);
+    return boton;
+}
+
+function pasarAHexadecimal(num){
+    let resultado=num.toString(16);
+    //debido a que da problemas con los colores si cada 
+    // color no tiene 2 dígitos hexadecimales, hay que añadir 0 por la izda
+    if (resultado.length ===1) resultado="0"+resultado;
+    if (resultado.length ===0) resultado="00";
+    return resultado;
+}
+/*
+function desplazarBolas(){  // no usamos esta función
+    let arrayEliminados=Bola.arrayBolas.splice(0, Bola.arrayBolas.length);
+    Bola.arrayBolas.forEach((bola, indice, array)=>{
+        //bola.desplazar(); comn método en clase Bola, no lo usamos
+        bola.eliminar();
+        arrayEliminados.forEach((bola)=>{
+            let nuevaBola= new Bola(bola.radio, bola.posX+10, bola.PosY+5, 
+                    bola.colorBola, bola.linearGradient);
+            nuevaBola.visualizar();
+});
+*/
+function desplazar2(){
+    Bola.arrayBolas.forEach((bola)=>{
+        console.log(bola);
+        const divBola=document.getElementById(bola.id);
+        bola.posX+=10;
+        bola.posY+=5;
+        divBola.style.left=`${bola.posX}px`;
+        divBola.style.top = `${bola.posY}px`;
+    });
+}
+
+
+function pararMovimiento(){
+    clearInterval(intervalo);
+}
+
+function iniciarMovimiento(){
+    intervalo=setInterval(desplazar2, 50);
+}
+
+function eliminarUltimo(){
+    if (Bola.arrayBolas.length >0){
+        const ultimaBola=Bola.arrayBolas.pop();
+        // console.log(ultimaBola);
+        ultimaBola.eliminar();
+    }
+}
+
+function escucharTeclas(evento){
+    console.log('tecla: ' + evento.key);
+    switch (evento.key){
+        case 'Home' : //tecla inicio
+            iniciarMovimiento();
+            break;
+        case 'End': // fin
+            pararMovimiento();
+        break;
+        case 'Delete': 
+            eliminarUltimo();
+            break;
+    }
+}
+
+export const misFunciones = {
+    formatearA3cifras,
+    generarRandomInt,
+    pasarAHexadecimal,
+    crearBotonCentrado,
+    escucharTeclas
+}
---
+++ TAREAS/ImagenesEnTabla/js/app.js
@@ -0,0 +1,47 @@
+let celdas = document.getElementsByTagName ('td');
+
+let img = document.createElement('img');
+img.setAttribute ('src', 'img/1.jpg');
+img.setAttribute ('alt', 'No se encuentra la imagen');
+img.setAttribute ('heigth', '200');
+img.setAttribute ('width', '200');
+
+celdas [0].appendChild(img);
+
+let img2 = document.createElement('img');
+img2.setAttribute ('src', 'img/2.jpg');
+img2.setAttribute ('alt', 'No se encuentra la imagen');
+img2.setAttribute ('heigth', '200');
+img2.setAttribute ('width', '200');
+
+celdas [1].appendChild(img2);
+
+
+let img3 = document.createElement('img');
+img3.setAttribute ('src', 'img/3.jpg');
+img3.setAttribute ('alt', 'No se encuentra la imagen');
+img3.setAttribute ('heigth', '200');
+img3.setAttribute ('width', '200');
+
+celdas [2].appendChild(img3);
+
+
+let img4 = document.createElement('img');
+img4.setAttribute ('src', 'img/4.jpg');
+img4.setAttribute ('alt', 'No se encuentra la imagen');
+img4.setAttribute ('heigth', '200');
+img4.setAttribute ('width', '200');
+
+celdas [3].appendChild(img4);
+
+
+let img5 = document.createElement('img');
+img5.setAttribute ('src', 'img/5.jpg');
+img5.setAttribute ('alt', 'No se encuentra la imagen');
+img5.setAttribute ('heigth', '200');
+img5.setAttribute ('width', '200');
+
+celdas [4].appendChild(img5);
+
+
+
