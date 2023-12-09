d382a6f62f82f51286515de9a42d8a1576487e73
rguindi <raul_fv@hotmail.com>
2023-12-09 16:01:09 +0100

    22

--- TAREAS/EJERCICIO22/js/funciones.js
+++ TAREAS/EJERCICIO22/js/funciones.js
@@ -1,3 +1,16 @@
+// Al cargar la página cargamos la configuracion
+window.onload = function(){
+    var dinero = sessionStorage.getItem('dinero');
+    var apuesta = sessionStorage.getItem('apuesta');
+    var fondo = sessionStorage.getItem('fondo');
+    document.getElementById("Restante").innerHTML = dinero;
+    document.getElementById("btn-apuesta").innerText = apuesta;
+    document.getElementById("Apostado").innerText = apuesta;
+    document.getElementById("juego").style.backgroundColor = fondo;
+
+
+};
+
 //Número aleatorio entre 1 y 6:  
 
 function generarRandomInt(max){     
@@ -14,24 +27,33 @@ function moverDados(){
     dado2.innerText = numeroAleatorio2;
 
     const audio = document.getElementById("audioLanzar");  
-    audio.play();
+    console.log(sessionStorage.getItem("activaSonido"));
+    if(sessionStorage.getItem("activaSonido")   == "true" ){
+        audio.play();
+    }
 
 
     //CONSTANTES DE DINERO
         const dinero = document.getElementById("Restante");
-        const apuesta = document.getElementById("btn-apuesta").getAttribute("value");
+        const apuesta = document.getElementById("btn-apuesta").innerText;
+      
 
 
     //GANA
     if(numeroAleatorio1+numeroAleatorio2 == 7 || numeroAleatorio1+numeroAleatorio2 == 11){
         const audio = document.getElementById("audioGanar");  
-        audio.play();
+        if(sessionStorage.getItem("activaSonido") == "true"){
+            audio.play();
+        }
+        
         dinero.innerText = parseInt(dinero.innerText) + parseInt(apuesta);
     }
     //PIERDE
     else if(numeroAleatorio1+numeroAleatorio2 == 2 || numeroAleatorio1+numeroAleatorio2 == 3 || numeroAleatorio1+numeroAleatorio2 == 12){
         const audio = document.getElementById("audioPerder");  
-        audio.play();
+        if(sessionStorage.getItem("activaSonido") == "true"){
+            audio.play();
+        }
         dinero.innerText = parseInt(dinero.innerText) - (parseInt(apuesta)*2);
     }
 
@@ -43,8 +65,14 @@ function moverDados(){
 
 }
 
-function empezar(){
-    document.body.style.backgroundColor = "black";
+function configurar(){
+    sessionStorage.setItem("dinero", document.getElementById("dineroRestante").value);
+    sessionStorage.setItem("apuesta", document.getElementById("valorApuesta").value);
+    sessionStorage.setItem("activaSonido", document.getElementById("activaSonido").checked);
+    sessionStorage.setItem("fondo", document.getElementById("fondo").value);
+    window.location.href = "./index.html";
+    
+
 }
 
 function colorFondo() {
