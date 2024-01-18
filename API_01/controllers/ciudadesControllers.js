b28b85cc5565ee656056d0e06ee29ccebebf59de
rguindi <raul@hotmail.com>
2024-01-18 18:04:53 +0100

    funciona

--- API_01/controllers/ciudadesControllers.js
+++ API_01/controllers/ciudadesControllers.js
@@ -4,7 +4,7 @@ const db = require ('../database/db'); //No requiere extension js
 const getCiudades = (req,res)=>{         //localhost:3000/ciudades
     db.query('SELECT * FROM ciudades', (err, resultados)=>{
         if(err){
-            console.log('Error al obtener los datos', err);
+            console.error('Error al obtener los datos', err);
         }else{
             res.json(resultados);
         }
@@ -12,12 +12,31 @@ const getCiudades = (req,res)=>{         //localhost:3000/ciudades
 
 };
 
+const getCiudadById = (req, res) => { //http://localhost:3000/ciudades/registro/3
+    const idRegistro = req.params.id;
+ 
+    // Consulta a la base de datos para obtener el registro por ID
+    db.query('SELECT * FROM ciudades WHERE id = ?', [idRegistro], (err, resultados) => {
+      if (err) {
+        console.error('Error al obtener el registro desde la base de datos:', err);
+        res.status(500).json({ error: 'Error interno del servidor' });
+      } else {
+        // Verifica si se encontró un registro
+        if (resultados.length > 0) {
+          res.json(resultados[0]); // Devuelve el primer resultado encontrado (debería ser único)
+        } else {
+          res.status(404).json({ error: 'Registro no encontrado' });
+        }
+      }
+    });
+  };
+
 //Funcion insertar ciudades
 const crearCiudad = (req,res)=>{
 const {nuevoNombre, habitantes} = req.body;
 db.query( 'INSERT INTO ciudades (nombre, cantidad) VALUES (?,?)',[nuevoNombre, habitantes],(err,resultado)=>{
     if(err){
-        console.log('Error al guardar los datos', err);
+        console.error('Error al guardar los datos', err);
         res.status(500).json({error:'Error interno en el servidor'});
     } else{
         res.json({recibido:true, nuevoNombre, habitantes, id: resultado.insertid})
@@ -26,7 +45,74 @@ db.query( 'INSERT INTO ciudades (nombre, cantidad) VALUES (?,?)',[nuevoNombre, h
 };
 
 
+//modificar ciudad
+const putCiudad = (req,res)=>{
+    const idRegistro = req.params.id;
+    const {nuevoNombre, habitantes} = req.body;
+    const sql = 'UPDATE ciudades SET nombre = ?, cantidad = ? WHERE ID = ?';
+    db.query(sql, [nuevoNombre, habitantes, idRegistro], (err, resultado)=>{
+        if(err){
+            console.error('Error al guardar los datos', err);
+            res.status(500).json({error:'Error interno en el servidor'});
+        } else{
+            res.json({recibido:true, nuevoNombre, habitantes, id: resultado.idRegistro})
+
+        }
+    });
+}
+
+//modificar ciudad
+const patchCiudad = (req,res)=>{
+    const idRegistro = req.params.id;
+    const {nuevoNombre, habitantes} = req.body;
+    const sql = 'UPDATE ciudades SET nombre = ?, cantidad = ? WHERE ID = ?';
+    db.query(sql, [nuevoNombre, habitantes, idRegistro], (err, resultado)=>{
+        if(err){
+            console.error('Error al guardar los datos', err);
+            res.status(500).json({error:'Error interno en el servidor'});
+        } else{
+            res.json({recibido:true, nuevoNombre, habitantes, id: resultado.idRegistro})
+
+        }
+    });
+}
+
+
+//modificar ciudad
+const actualizarCiudad = (req,res)=>{  //http://localhost:3000/ciudades/3
+    const idRegistro = req.params.id;
+    const {nuevoNombre, habitantes} = req.body;
+    const updatedFields = [];
+    const updatedValues = [];
+    if (nuevoNombre!=undefined) {
+        updatedValues.push(nuevoNombre);
+        updatedFields.push('nombre =?');
+    }
+    if (habitantes!=undefined) {
+        updatedValues.push(habitantes);
+        updatedFields.push('cantidad =?');
+    }
+
+
+    const sql = `UPDATE ciudades SET ${updatedFields.join(', ')} WHERE id =?`;
+    const queryValues = [...updatedValues, idRegistro];
+
+    db.query(sql, queryValues, (err, resultado)=>{
+        if(err){
+            console.error('Error al guardar los datos', err);
+        } else{
+            res.json({recibido:true, nuevoNombre, habitantes, id: resultado.idRegistro})
+
+        }
+    });
+}
+
+
 module.exports={
     getCiudades,
     crearCiudad,
+    getCiudadById,
+    putCiudad,
+    patchCiudad,
+    actualizarCiudad
 };
--- API_01/rutas/rutas_ciudades.js
+++ API_01/rutas/rutas_ciudades.js
@@ -5,7 +5,11 @@ const ciudadesController= require('../controllers/ciudadesControllers');
 //Ruta para obtener datos de la BD
 //Cambiamos app por la nueva constante rutasciudades
 rutasCiudades.get('/', ciudadesController.getCiudades);
-rutasCiudades.post('/add', ciudadesController.crearCiudad);
+rutasCiudades.post('/add', ciudadesController.crearCiudad);  //Lo normal es usar la misma ruta para el GET y EL POST
+rutasCiudades.get('/:id', ciudadesController.getCiudadById);  //:id indica q es un parametro
+rutasCiudades.put('/put/:id', ciudadesController.putCiudad); 
+rutasCiudades.patch('/patch/:id', ciudadesController.patchCiudad); 
+rutasCiudades.patch('/:id', ciudadesController.actualizarCiudad); 
 
 //exportamos la constante routes
 module.exports = rutasCiudades;
